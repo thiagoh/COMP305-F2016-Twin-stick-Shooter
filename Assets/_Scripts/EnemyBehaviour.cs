ea8fb7ef457b77454a1dee2a8db46ff567b9b94a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-09-24 10:16:14 -0400

    Initial Commit

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	// How many times should I be hit before I die
+	public int health = 2;
+
+	// When the enemy dies, we play an explosion
+	public Transform explosion;
+
+	// What sound to play when hit
+	public AudioClip hitSound;
+
+	// create an AudioSource variable
+	private AudioSource audio;
+
+	private GameController controller;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		controller = 
+			GameObject
+				.FindGameObjectWithTag("GameController")
+				.GetComponent<GameController>();
+	}
+
+	void OnCollisionEnter2D(Collision2D theCollision)
+	{
+		// Uncomment this line to check for collision
+		//Debug.Log("Hit"+ theCollision.gameObject.name);
+		// this line looks for "laser" in the names of
+		// anything collided.
+		if(theCollision.gameObject.name.Contains("Laser"))
+		{
+			LaserBehaviour laser =
+				theCollision.gameObject.GetComponent
+				("LaserBehaviour") as LaserBehaviour;
+			health -= laser.damage;
+			Destroy (theCollision.gameObject);
+
+			// Plays a sound from this object's AudioSource
+			audio.PlayOneShot(hitSound, 1.0f);
+		}
+
+		if (theCollision.gameObject.name.Contains ("Player Ship")) {
+			controller.DecreaseLives (1);
+		}
+
+		if (health <= 0)
+		{
+			// Check if explosion was set
+			if(explosion)
+			{
+				GameObject exploder = ((Transform)Instantiate(explosion, this.
+					transform.position, this.transform.rotation)).gameObject;
+				Destroy(exploder, 2.0f);
+			}
+				
+			controller.KilledEnemy();
+			controller.IncreaseScore(10);
+
+
+			Destroy (this.gameObject);
+		}
+	}
+}
+
---
+++ Assets/_Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+
+	private Transform player;
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player Ship").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 delta = player.position - transform.position;
+		delta.Normalize();
+		float moveSpeed = speed * Time.deltaTime;
+		transform.position = transform.position + (delta * moveSpeed);
+	}
+}
+
